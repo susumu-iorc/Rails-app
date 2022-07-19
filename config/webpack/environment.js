70da8d51f38a3931be9dd0ba6068dc8ff2c10235
susumu <susumu@iorc.net>
2022-07-19 22:34:09 +0900

    とりあえずヘッダー作ったけどbootstrapがわからないからスãデザインは後ほど

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
  const webpack = require('webpack')
   environment.plugins.append(
     'Provide',
@@ -11,3 +9,5 @@ module.exports = environment
       Popper: ['popper.js', 'default']
     })
   )
+  
+module.exports = environment
