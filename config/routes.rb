3b60585f3cf1f65a6c65e297bad8069c396b3c7c
susumu <susumu@iorc.net>
2022-07-27 00:33:59 +0900

    インデントとか少し調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "show", to: "shop_list#show"
+  #get "show", to: "shop_list#show"
   get 'static_pages/home'
-  #root 'static_pages#home'
+  #root 'static_pages#home' #そのうちコントローラーごと削除する
   root 'shop_list#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
