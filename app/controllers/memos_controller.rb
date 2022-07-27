53c76e1f08fe5ac11e77d8913f9bc6d9cb990c2f
susumu <s.osawa@famitra.jp>
2022-07-27 16:24:51 +0900

    Memoページを表示

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,7 @@
 class MemosController < ApplicationController
   def show
-    @pageid = params[:place_id]
+    @place_id = params[:place_id]
+    @shop   = Shop.find_by(place_id: @place_id)
+    @memo   = Memo.find_by(place_id: @place_id, user_id: current_user.id)
   end
 end
