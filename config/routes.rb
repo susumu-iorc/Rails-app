d99a8067a467c9ca8a147294f50430f0a3933aff
susumu <susumu@iorc.net>
2022-08-02 17:32:56 +0900

    住所登録リダしてない場合の処理とホームをダミーデータãでなくて実際のapi叩くように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   #get "show", to: "shop_list#show"
-  get 'static_pages/home'
+  # get 'static_pages/home'
   #root 'static_pages#home' #そのうちコントローラーごと削除する
   root 'shop_list#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
