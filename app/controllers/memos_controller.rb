22e18984a4fbea1a26bbe008ffbbc8fbbf1dc97b
susumu <susumu@iorc.net>
2022-08-02 12:40:37 +0900

    memosのフォーãの更新

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -4,4 +4,17 @@ class MemosController < ApplicationController
     @shop   = Shop.find_by(place_id: @place_id)
     @memo   = Memo.find_by(place_id: @place_id, user_id: current_user.id)
   end
+
+  def update
+    puts "PLACE => #{params[:place_id]}, USER => #{params[:user_id]}, MEMO => #{params[:memo]}"
+    #if current_user.id == params[:user_id]
+      @memo = Memo.find_by(place_id: params[:place_id], user_id: current_user.id)
+      puts "FAVORITE => #{@memo.favorite}"
+      if @memo.update(memo:params[:memo])
+        # 更新に成功した場合を扱う。
+        puts "SEIKO========================"
+        redirect_to request.referer
+      end
+     #end
+  end
 end
