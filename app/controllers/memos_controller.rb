e0a833658c587c55aa46d421146e9c764887dd31
susumu <susumu@iorc.net>
2022-08-02 13:12:10 +0900

    行った回数増加、お気に入り変更

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -7,7 +7,9 @@ class MemosController < ApplicationController
 
   def update
     puts "PLACE => #{params[:place_id]}, USER => #{params[:user_id]}, MEMO => #{params[:memo]}"
-    #if current_user.id == params[:user_id]
+
+    # メモの更新
+    if !params[:memo].nil?
       @memo = Memo.find_by(place_id: params[:place_id], user_id: current_user.id)
       puts "FAVORITE => #{@memo.favorite}"
       if @memo.update(memo:params[:memo])
@@ -15,6 +17,30 @@ class MemosController < ApplicationController
         puts "SEIKO========================"
         redirect_to request.referer
       end
-     #end
+    end
+
+    # 回数の更新
+    if !params[:count].nil?
+      @memo = Memo.find_by(place_id: params[:place_id], user_id: current_user.id)
+      puts "FAVORITE => #{@memo.favorite}"
+      if @memo.update(count: @memo.count + 1)
+        # 更新に成功した場合を扱う。
+        puts "COUNTUP!========================"
+        redirect_to request.referer
+      end
+    end
+
+    # お気に入り変更
+    if !params[:favo].nil?
+      @memo = Memo.find_by(place_id: params[:place_id], user_id: current_user.id)
+      if @memo.favorite < 3 then @memo.favorite += 1
+      else @memo.favorite = 0
+      end
+      if @memo.update(favorite: @memo.favorite)
+        # 更新に成功した場合を扱う。
+        puts "COUNTUP!========================"
+        redirect_to request.referer
+      end
+    end    
   end
 end
